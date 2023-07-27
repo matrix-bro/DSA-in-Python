2b44ed6eac36951272d83398cb2a2e67e128f27b
matrix-bro <razmhz11@gmail.com>
2023-07-27 11:20:32 +0545

    Symmetric-void pattern

---
+++ patterns/symmetric_void_pattern.py
@@ -0,0 +1,70 @@
+"""
+Input Format: N = 3
+Result: 
+******
+**  **
+*    *
+*    *
+**  **
+******
+
+
+Input Format: N = 6
+Result:   
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+
+"""
+
+n = 3
+i = n
+space = 0
+
+while i > 0:
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    
+    print(" " * space, end="")
+
+    j -= 1
+    while j > 0:
+        print("*", end="")
+        j -= 1
+    
+    print()
+    i -= 1
+    space += 2
+
+
+i = 1
+spaces = n * 2 - 2
+
+while i <= n:
+    j = 1
+
+    while j <= i:
+        print("*", end="")
+        j += 1
+
+    print(" " * spaces, end="")
+
+    j = i
+    while j >= 1:
+        print("*", end="")
+        j -= 1
+    
+    print()
+    i += 1
+    spaces -= 2
